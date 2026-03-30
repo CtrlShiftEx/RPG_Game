33255dc5e60843044c96253325f3636edf3b4610
Jake Allen <ctrlshiftex@gmail.com>
2026-03-30 15:19:53 -0400

    updated rooms and added some gameflow

--- game.py
+++ game.py
@@ -2,3 +2,12 @@ from character import Character, create_character
 
 hero = create_character()
 hero.display_stats()
+
+game_running = True
+
+while game_running:
+    command = input("\nWhat do you want to do?")
+
+    if command == "quit":
+        print("Goodbye!")
+        game_running = False
